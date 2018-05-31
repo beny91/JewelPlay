e85b596ca3337426319b3b7b359921e6e0338603
beny91 <sosik4@gamil.com>
2018-05-31 09:19:46 +0900

    add scripts

---
+++ Jewel Play/scripts/game.js
@@ -0,0 +1,20 @@
+jewel.game = (function(){
+  var dom = jewel.dom,
+    $ = dom.$;
+
+  // 현재 활성화된 스크린을 보이지 앟게 하고 지정된 ID를 가진 스크린을 표시한다.
+  function showScreen(screenId){
+    // sizzle
+    var activeScreen = $("#game.screen.active")[0],
+        screen = $("#" + screenId)[0];
+    if(activeScreen){
+      dom.removeClass(screen,"active");
+    }
+    dom.addClass(screen, "active");
+  }
+
+  //공용 메서드를 정의한다.
+  return {
+    showScreen : showScreen
+  };
+})();
